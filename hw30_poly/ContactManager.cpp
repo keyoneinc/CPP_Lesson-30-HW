084c46a347bcd341af0551897312a3e1550212b3
Yaroslav <59771733+keyoneinc@users.noreply.github.com>
2024-07-15 23:47:08 +0100

    putted ContactManager class defenition in a cpp file, added new void editContact

--- hw30_poly/ContactManager.cpp
+++ hw30_poly/ContactManager.cpp
@@ -1,1 +1,40 @@
 #include "ContactManager.h"
+
+void ContactManager::addContact(Contact contact)
+{
+	contacts.push_back(contact);
+}
+
+void ContactManager::editContact(const string& name)
+{
+    for (auto& contact : contacts) {
+        if (contact.getName() == name) {
+            contact.editContact();
+            cout << "Contact edited.\n";
+            return;
+        }
+    }
+    cout << "Contact not found.\n";
+}
+
+void ContactManager::removeContact(const string& name)
+{
+    auto it = remove_if(contacts.begin(), contacts.end(), [&](const Contact& contact) {
+        return contact.getName() == name;
+        });
+    if (it != contacts.end()) {
+        contacts.erase(it, contacts.end());
+        cout << "Contact removed.\n";
+    }
+    else {
+        cout << "Contact not found.\n";
+    }
+}
+
+void ContactManager::showContacts() const
+{
+    for (const auto& contact : contacts) {
+        contact.showInfo();
+        cout << "\n";
+    }
+}
--- hw30_poly/ContactManager.h
+++ hw30_poly/ContactManager.h
@@ -14,29 +14,12 @@ private:
 	vector<Contact> contacts;
 
 public:
-    void addContact(Contact contact) {
-        contacts.push_back(contact);
-    }
+    void addContact(Contact contact);
+    void editContact(const string& name);
 
-    void removeContact(const string& name) {
-        auto it = remove_if(contacts.begin(), contacts.end(), [&](const Contact& contact) {
-            return contact.getName() == name;
-            });
-        if (it != contacts.end()) {
-            contacts.erase(it, contacts.end());
-            cout << "Contact removed.\n";
-        }
-        else {
-            cout << "Contact not found.\n";
-        }
-    }
+    void removeContact(const string& name);
 
-    void showContacts() const {
-        for (const auto& contact : contacts) {
-            contact.showInfo();
-            cout << "\n";
-        }
-    }
+    void showContacts() const;
 
 };
 
